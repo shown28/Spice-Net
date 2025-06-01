5ff5d21fec65dd09cf217fad104daede47e86dd8
Shown <manuelshown@gmail.com>
2025-06-01 19:54:50 +0530

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,29 @@
+
+const Home = () => {
+  return (
+    <>
+        {/* hero section */}
+        <div  className='setBG w-full h-screen flex flex-col items-center justify-center ' >
+
+            <h1 className='text-white text-3xl md:text-4xl text-center font-bold mb-5'>Pure Spice. Honest Taste <br />
+                    Strate from Kerala</h1>
+            <div className="text-white flex flex-col md:flex-row">
+                <a href='' className="mb-3 py-2 text-xl px-2 text-center md:mx-2 bg-green-600 rounded hover:bg-lime-400 hover:text-slate-900">Kashmiri Chilly Powder <i className="fa-solid fa-angle-right"></i></a>
+                <a href='' className="mb-3 py-2 text-xl px-2 text-center md:mx-2 bg-green-600 rounded hover:bg-lime-400 hover:text-slate-900  ">Turmeric Powder <i className="fa-solid fa-angle-right"></i></a> 
+                <a href='' className="mb-3 py-2 text-xl px-2 text-center md:mx-2 bg-green-600 rounded hover:bg-lime-400 hover:text-slate-900  ">Rice Hour Powder <i className="fa-solid fa-angle-right"></i></a>
+            </div>
+
+        </div>
+        {/* about */}
+        <div className='w-full h-screen'>
+            
+                {/* about content */}
+
+                {/* abt-img */}
+            
+        </div>
+    </>
+  )
+}
+
+export default Home
